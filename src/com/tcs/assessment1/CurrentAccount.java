412da30c30a25d3531aa0377778a774cc3c6c410
deepp <tcsdeepp@gmail.com>
2021-08-11 12:38:54 +0000

    Assessment-1

---
+++ src/com/tcs/assessment1/CurrentAccount.java
@@ -0,0 +1,15 @@
+package com.tcs.assessment1;
+
+import java.util.Date;
+
+public class CurrentAccount extends Account{
+	
+	private double overDraftBalance;
+	public CurrentAccount(double balance, String ownerName, Date createDate, String type, String status, double overDraftBalance) {
+		super(balance, ownerName, createDate, type, status);
+		this.overDraftBalance = overDraftBalance;
+	}
+	public double getOverDraftBalance() {
+		return overDraftBalance;
+	}
+}
---
+++ src/com/tcs/assessment1/SavingsAccount.java
@@ -0,0 +1,10 @@
+package com.tcs.assessment1;
+
+import java.util.Date;
+
+public class SavingsAccount extends Account{
+
+	public SavingsAccount(double balance, String ownerName, Date createDate, String type, String status) {
+		super(balance, ownerName, createDate, type, status);
+   }
+}
